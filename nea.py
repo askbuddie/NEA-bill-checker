dc56d2b1b26f2d01af819d70f2a1c34ecd8ee308
Pradeep Bhattarai <29733866+pr0d33p@users.noreply.github.com>
2019-06-28 23:45:22 +0545

    Fixed Customer issue

--- nea.py
+++ nea.py
@@ -12,7 +12,7 @@ custid = raw_input("Enter Customer id: ")
 
 add = raw_input("Enter your NEA Location as in your Bill: ").upper()
 
-file = 'address.txt'
+file = 'db/a.txt'
 
 with open(file) as a:
 	x = a.read()
@@ -26,20 +26,29 @@ with open(file) as a:
 		else:
 			address = ''.join(re.findall(address1 + " =.*", x)).replace(address1 + " = ", '')
 
-parameters = {'NEA_location':address, 'sc_no': sc, 'consumer_id': custid, 'Fromdatepicker': '2/28/2017', 'Todatepicker': date}
-# # print parameters
-
-r = requests.post('https://www.neabilling.com/viewonline/viewonlineresult/', params=parameters)
-# # print(r.url)
-if 'No Records' in r.text:
-	print "Some of your information is incorrect. Records not found."
-else:
-	a = "".join(re.findall("Customer .*", r.text))
-	name = a[45:][::-1][6:][::-1]
-	print name
-
-	if 'UN-PAID' in r.text:
-		print "Amount not paid!"
 	else:
-		print "Amount has been paid!"
-# print r.text
+		print add + " Not found!"
+		exit(1)
+if (address == '' or sc == '' or custid == ''):
+	print "You forgot to enter something!"
+	exit(1)
+else:
+	parameters = {'NEA_location':address, 'sc_no': sc, 'consumer_id': custid, 'Fromdatepicker': '2/28/2017', 'Todatepicker': date}
+	# # print parameters
+
+	r = requests.post('https://www.neabilling.com/viewonline/viewonlineresult/', params=parameters)
+	# # print(r.url)
+	if 'No Records' in r.text:
+		print "Some of your information is incorrect. Records not found."
+	else:
+		a = "".join(re.findall("Customer .*", r.text))
+		name = a[45:][::-1][6:][::-1]
+		print name
+
+		if 'UN-PAID' in r.text:
+			print "Amount not paid!"
+		elif 'ConsumerId must be' in r.text:
+			print "Please eter your numeric Customer ID."
+		else:
+			print "Amount has been paid!"
+	# print r.text
